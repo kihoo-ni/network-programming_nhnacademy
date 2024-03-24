b40874a0dc779200781128e10d31602abf8f61d6
kihoo-ni <gudcjf532@gmail.com>
2024-03-24 17:29:58 +0900

    3.24 빙고게임

---
+++ socket/src/main/java/com/nhnacademy/Bingo.java
@@ -0,0 +1,232 @@
+package com.nhnacademy;
+
+import java.util.Scanner;
+
+public class Bingo {
+    String[][] bingo;
+    int bingoCount;
+    public static int bingoLineNumber;
+    public static String[] checkNumArray = new String[bingoLineNumber * bingoLineNumber];
+
+   
+
+    public Bingo(int bingoLineNumber) {
+        this.bingoLineNumber = bingoLineNumber;
+        bingo = new String[bingoLineNumber][bingoLineNumber];
+        checkNumArray = new String[bingoLineNumber * bingoLineNumber];
+     
+    }
+
+    public static void checkNUmArrayInputNum(){
+          //빙고 숫자판에 고르는 숫자의 중복을 방지하기위해 배열초기화
+          for(int i = 0;i<bingoLineNumber*bingoLineNumber;i++){checkNumArray[i] = Integer.toString(i + 1);}
+    }
+
+    public void insertBingoNumber() {
+        System.out.println("----------------------------------------------------------");
+        System.out.println("빙고판에 숫자를 입력합니다. 숫자는 좌우 위아래 순으로 입력");
+        System.out.println("----------------------------------------------------------");
+        System.out.println("숫자는 본인이 선택한 빙고 " + bingoLineNumber + "줄수 기준으로 1부터 " + bingoLineNumber * bingoLineNumber
+                + " 이하의 숫자로 입력");
+        try (
+                Scanner scanner = new Scanner(System.in);
+
+        ) {
+
+            for (int row = 0; row < bingoLineNumber; row++) {
+                for (int col = 0; col < bingoLineNumber; col++) {
+                    if (col == bingoLineNumber - 1) {
+                        System.out.println("열의 마지막 숫자를 입력해주세요");
+                        System.out.println("____");
+                    } else {
+
+                        System.out.println("숫자를 입력해주세요");
+                        System.out.println("___");
+                    }
+
+                    int num = scanner.nextInt();
+                    bingo[row][col] = Integer.toString(num);
+                }
+            }
+          
+        } catch (Exception ignore) {
+        }
+    }
+
+    public void insertComputerBingoNumber() {
+        int lastnum = bingoLineNumber * bingoLineNumber;
+
+        String[] selectNumbers = new String[lastnum];
+
+        for (int i = 0; i < lastnum; i++) {
+            selectNumbers[i] = Integer.toString(i + 1);
+        }
+
+        for (int row = 0; row < bingoLineNumber; row++) {
+            for (int col = 0; col < bingoLineNumber; col++) {
+
+                int num = (int) (Math.random() * lastnum);
+                String selectNum = selectNumbers[num];
+                if (selectNum == null) {
+                    col--;
+                    continue;
+                }
+
+                selectNumbers[num] = null;
+                bingo[row][col] = selectNum;
+
+            }
+        }
+
+    }
+
+    public String showMeTheBingo() {
+        String bingoArray = new String();
+
+        for (int row = 0; row < bingoLineNumber; row++) {
+            for (int col = 0; col < bingoLineNumber; col++) {
+                if (col == bingoLineNumber - 1) {
+
+                    bingoArray += bingo[row][col] + "\n";
+                } else {
+
+                    bingoArray += bingo[row][col] + " ";
+                }
+            }
+        }
+        return bingoArray;
+    }
+
+    public void checkBingoNumber(Bingo otherBingo, boolean computer) {
+        boolean isComputer = computer;
+     
+        
+        try (
+                Scanner scanner = new Scanner(System.in);) {
+
+            if (!isComputer) {
+
+                System.out.print("숫자를 고르세요 : ");
+                int pickNum = scanner.nextInt();
+                scanner.nextLine();
+                String pickNumToStr = Integer.toString(pickNum);
+                checkNumArray[pickNum - 1] = null;
+                System.out.println("for문 전");
+                for (int i = 0; i < bingoLineNumber; i++) {
+                    for (int j = 0; j < bingoLineNumber; j++) {
+                        if (pickNumToStr.equals(bingo[i][j]) && (!bingo[i][j].equals("O"))
+                                && (!bingo[i][j].equals("X"))) {
+
+                            bingo[i][j] = "O";
+                            break;
+                        }
+                    }
+                }
+                for (int i = 0; i < bingoLineNumber; i++) {
+                    for (int j = 0; j < bingoLineNumber; j++) {
+                        if (pickNumToStr.equals(otherBingo.bingo[i][j]) && (!otherBingo.bingo[i][j].equals("O"))
+                                && (!otherBingo.bingo[i][j].equals("X"))) {
+                            otherBingo.bingo[i][j] = "O";
+                            break;
+                        }
+                    }
+                }
+
+            } else {
+                int randomComNum = (int) Math.random() * (bingoLineNumber * bingoLineNumber) + 1;
+                String randomComStr = Integer.toString(randomComNum);
+                for (int i = 0; i < bingoLineNumber * bingoLineNumber; i++) {
+
+                    if (checkNumArray[i] == null) {
+                        continue;
+                    } else {
+                        while (randomComStr.equals(checkNumArray[i])) {
+                            randomComStr = checkNumArray[i];
+                            checkNumArray[i] = null;
+                            break;
+                        }
+                        break;
+                    }
+                }
+
+                for (int i = 0; i < bingoLineNumber; i++) {
+                    for (int j = 0; j < bingoLineNumber; j++) {
+                        if (randomComStr.equals(bingo[i][j]) && (!bingo[i][j].equals("O"))
+                                && (!bingo[i][j].equals("X"))) {
+                            bingo[i][j] = "X";
+                            break;
+                        } else {
+                            j--;
+                            continue;
+                        }
+                    }
+                }
+                for (int i = 0; i < bingoLineNumber; i++) {
+                    for (int j = 0; j < bingoLineNumber; j++) {
+                        if (randomComStr.equals(otherBingo.bingo[i][j]) && (!otherBingo.bingo[i][j].equals("O"))
+                                && (!otherBingo.bingo[i][j].equals("X"))) {
+                            otherBingo.bingo[i][j] = "X";
+                            break;
+                        }
+                    }
+                }
+
+            }
+
+            System.out.println("___");
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public void detectBingo(String bingoSymbol) {
+        int diagonalCountBingo = 0;
+        int horizontalCountBingo = 0;
+        int verticalCountBingo = 0;
+
+        for (int i = 0; i < bingoLineNumber; i++) {
+            if (bingo[i][i].equals(bingoSymbol)) {
+                diagonalCountBingo += 1;
+            }
+        }
+        if (diagonalCountBingo == bingoLineNumber) {
+            bingoCount += 1;
+        }
+        for (int i = 0; i < bingoLineNumber; i++) {
+
+            for (int k = 0; k < bingoLineNumber; k++) {
+                if (bingo[i][k].equals(bingoSymbol)) {
+                    horizontalCountBingo += 1;
+
+                    if (k == bingoLineNumber - 1 && (horizontalCountBingo % bingoLineNumber) == 0) {
+                        bingoCount += 1;
+                    } else if (k == bingoLineNumber - 1) {
+                        horizontalCountBingo = 0;
+                    }
+                }
+            }
+
+        }
+
+        for (int k = 0; k < bingoLineNumber; k++) {
+            for (int i = 0; i < bingoLineNumber; i++) {
+                if (bingo[i][k].equals(bingoSymbol)) {
+                    verticalCountBingo += 1;
+                    if (i == bingoLineNumber - 1 && (verticalCountBingo % bingoLineNumber) == 0) {
+                        bingoCount += 1;
+                    } else if (k == bingoLineNumber - 1) {
+                        verticalCountBingo = 0;
+                    }
+                }
+            }
+        }
+
+    }
+
+    public int returnBingoCount() {
+
+        return bingoCount;
+    }
+}
